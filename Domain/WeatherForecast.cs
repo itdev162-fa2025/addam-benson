22677617b2968f1279a88db2c4c4f5e380e8760f
Addam Benson <addam.benson@gmail.com>
2024-09-14 15:40:23 -0500

    added WeatherForecast model

---
+++ Domain/WeatherForecast.cs
@@ -0,0 +1,13 @@
+
+public class WeatherForecast
+{
+    public int Id {get; set;}
+
+    public DateOnly Date {get; set;}
+
+    public int TemperatureC {get; set;}
+
+    public int TemperatureF {get; set;}
+
+    public string? Summary {get; set;}
+}
